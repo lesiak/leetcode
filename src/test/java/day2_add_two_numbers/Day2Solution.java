aa1908fc13ee0d4bec09e5c419104f88bdf0e453
lesiak <lesiak@outlook.com>
2022-09-09 20:49:50 +0200

    #3 Add two numbers as test

--- src/test/java/day2_add_two_numbers/Day2Solution.java
+++ src/test/java/day2_add_two_numbers/Day2Solution.java
@@ -1,5 +1,12 @@
 package day2_add_two_numbers;
 
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class ListNode {
     int val;
     ListNode next;
@@ -20,15 +27,30 @@ class ListNode {
     public String toString() {
         return "" + val + ((next != null) ? "," + next : "");
     }
+
+    List<Integer> toList() {
+        var ret = new ArrayList<Integer>();
+        this.appendToList(ret);
+        return ret;
+    }
+
+    private void appendToList(List<Integer> result) {
+        result.add(val);
+        if (next != null) {
+            next.appendToList(result);
+        }
+    }
 }
 
 public class Day2Solution {
 
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
         ListNode num1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode num2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-
+        // Assertions.assertThat(result).containsExactly(3);
         System.out.println(addTwoNumbers(num1, num2));
+        Assertions.assertThat(addTwoNumbers(num1, num2).toList()).containsExactly(7, 0, 8);
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
