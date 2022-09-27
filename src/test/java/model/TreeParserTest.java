704537099a40242f90780d82a19624c7b2dbea3a
lesiak <lesiak@outlook.com>
2022-09-27 22:13:23 +0200

    [JAVA] Broken TreeParser test

--- src/test/java/model/TreeParserTest.java
+++ src/test/java/model/TreeParserTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -25,5 +26,23 @@ public class TreeParserTest {
         assertThat(root.right.right.val).isEqualTo(7);
         assertThat(root.right.right.left).isNull();
         assertThat(root.right.right.right).isNull();
+    }
+
+    @Test
+    @Disabled
+    void testParseInputEmptyRightmostNode() {
+        var input = Arrays.asList(1,null,2, 3);
+        var root = TreeParser.parseInput(input);
+        assertThat(root.val).isEqualTo(1);
+//        assertThat(root.left.val).isEqualTo(9);
+//        assertThat(root.left.left).isNull();
+//        assertThat(root.left.right).isNull();
+//        assertThat(root.right.val).isEqualTo(20);
+//        assertThat(root.right.left.val).isEqualTo(15);
+//        assertThat(root.right.left.left).isNull();
+//        assertThat(root.right.left.right).isNull();
+//        assertThat(root.right.right.val).isEqualTo(7);
+//        assertThat(root.right.right.left).isNull();
+//        assertThat(root.right.right.right).isNull();
     }
 }
